3ddde85254ae0cf686cd6774fed12092013494f3
Interborn <dylansafra@icloud.com>
2023-01-13 17:06:34 -0700

    Edited server URL

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codr.onrender.com', {
+    const response = await fetch('https://jarvis-programming-assistant.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
