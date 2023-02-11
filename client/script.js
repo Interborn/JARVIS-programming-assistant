736286618194358aaeb7e6f0147f9ff4256dd906
Interborn <dylansafra@icloud.com>
2023-02-11 12:54:44 -0700

    Fixed API error.

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     // === CHANGE THIS TO YOUR OWN SERVER TO HANDLE THE REQUESTS === //
-    const response = await fetch('https://codr.onrender.com', {
+    const response = await fetch('https://jarvis-programming-assistant.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
